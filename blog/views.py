c34d88dce2301b4d7acd66fb1f8b75333f65500e
pot1223 <59383800+pot1223@users.noreply.github.com>
2020-01-16 06:12:55 +0900

    커밋10

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-from django.shortcuts import render
-from .models import Post
-from django.utils import timezone
-
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
